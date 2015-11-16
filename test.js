c5026617512e77a4d8cb3f5cb962de047c741cd4
Jan Rupnik <jan.rupnik@ijs.si>
2015-11-16 16:52:55 +0000

    test output

--- test.js
+++ test.js
@@ -3,9 +3,9 @@ var nodeimu  = require('./index.js');
 var IMU = new nodeimu.IMU();
 function dispAccel() { 
   var accel = IMU.getValue();
-  var sx = accel.x >= 0 ? '+' : '';
-  var sy = accel.y >= 0 ? '+' : '';
-  var sz = accel.z >= 0 ? '+' : '';
+  var sx = accel.x >= 0 ? ' ' : '';
+  var sy = accel.y >= 0 ? ' ' : '';
+  var sz = accel.z >= 0 ? ' ' : '';
   var str = util.format('%s%s %s%s %s%s', sx, accel.x.toFixed(4), sy, accel.y.toFixed(4), sz, accel.z.toFixed(4));
   console.log(str);
 }
