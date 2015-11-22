459cfd8b9d8eb7480274e8666fa7f417211507ae
Stéphane Lavirotte <stephane@lavirotte.com>
2015-11-22 08:43:16 +0100

    Check if specific fields exists in data

--- test.js
+++ test.js
@@ -6,7 +6,6 @@ var num = 0;
 var numStop = 100;
 console.time("async");
 
-
 var tic = new Date();
 var callb = function (e, data) {
   var toc = new Date();
@@ -20,7 +19,10 @@ var callb = function (e, data) {
   var sy = data.accel.y >= 0 ? ' ' : '';
   var sz = data.accel.z >= 0 ? ' ' : '';
   var str = util.format('%s%s %s%s %s%s', sx, data.accel.x.toFixed(4), sy, data.accel.y.toFixed(4), sz, data.accel.z.toFixed(4));
-  var str2 = util.format(' %s %s %s', data.temperature.toFixed(4), data.pressure.toFixed(4), data.humidity.toFixed(4));
+  var str2 = "";
+  if (data.temperature && data.pressure && data.humidity) {
+    var str2 = util.format(' %s %s %s', data.temperature.toFixed(4), data.pressure.toFixed(4), data.humidity.toFixed(4));
+  }
   console.log(str + str2);
 
   
--- testSync.js
+++ testSync.js
@@ -14,8 +14,12 @@ function dispAccel() {
   var sx = data.accel.x >= 0 ? ' ' : '';
   var sy = data.accel.y >= 0 ? ' ' : '';
   var sz = data.accel.z >= 0 ? ' ' : '';
+
   var str = util.format('%s%s %s%s %s%s', sx, data.accel.x.toFixed(4), sy, data.accel.y.toFixed(4), sz, data.accel.z.toFixed(4));
-  var str2 = util.format(' %s %s %s', data.temperature.toFixed(4), data.pressure.toFixed(4), data.humidity.toFixed(4));
+  var str2 = "";
+  if (data.temperature && data.pressure && data.humidity) {
+    var str2 = util.format(' %s %s %s', data.temperature.toFixed(4), data.pressure.toFixed(4), data.humidity.toFixed(4));
+  }
   console.log(str + str2);
   num++;
   if (num == numStop) {
