7e4d55bf46ae67ab95ad4c516f1d4b9838f6ddd7
Stéphane Lavirotte <stephane@lavirotte.com>
2016-04-07 14:21:16 +0200

    Add utilities functions from titlHeading to Heading in Degree

--- test.js
+++ test.js
@@ -14,6 +14,32 @@ var print_vector3 = function(name, data) {
   return util.format('%s: %s%s %s%s %s%s ', name, sx, data.x.toFixed(4), sy, data.y.toFixed(4), sz, data.z.toFixed(4));
 }
 
+var headingCorrection = function(heading, offset) {
+  if (typeof offset ==='undefined')
+      offset = 0;
+
+  // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
+  // Find yours here: http://www.magnetic-declination.com/
+  var declinationAngle = 0.03106686;
+
+  heading += declinationAngle + offset;
+
+  // Correct for when signs are reversed.
+  if (heading < 0)
+    heading += 2 * Math.PI;
+
+  // Check for wrap due to addition of declination.
+  if (heading > 2 * Math.PI)
+    heading -= 2 * Math.PI;
+
+  return heading;
+}
+
+var headingToDegree = function(heading) {
+  // Convert radians to degrees for readability.
+  return heading * 180 / Math.PI;
+}
+
 var tic = new Date();
 var callb = function (e, data) {
   var toc = new Date();
@@ -28,13 +54,15 @@ var callb = function (e, data) {
   // str += print_vector3('Gyro', data.gyro)
   // str += print_vector3('Compass', data.compass)
   // str += print_vector3('Fusion', data.fusionPose)
-  // str += util.format('TiltHeading: %s ', data.tiltHeading);
+  str += util.format('TiltHeading: %s ', headingToDegree(headingCorrection(data.tiltHeading, Math.PI / 2)).toFixed(0));
 
   var str2 = "";
   if (data.temperature && data.pressure && data.humidity) {
     var str2 = util.format('%s %s %s', data.temperature.toFixed(4), data.pressure.toFixed(4), data.humidity.toFixed(4));
   }
-  console.log(str + str2);
+  if ((num % 10) == 0) {
+    console.log(str + str2);
+  }
 
   num++;
   if (num == numStop) {
